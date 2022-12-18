ad7fbe6072704d2f917b87ba4a50df22bda90094
Alihan Demirdaş <info.alihandemirdas@gmail.com>
2022-12-18 22:27:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
+"use strict";
 const text = document.getElementById("root");
-
-const initialText = "Welcome to Alihan Demirdaş's personal webpage!";
-
+const initialText = "Alihan Demirdaş";
 let currentText = [];
 let initialChance = 1;
-
 let timer;
-function start(){
-  timer = setInterval(() => {
-    initialText.split("").forEach(text => {
-      const random = Math.floor(Math.random() * initialChance + 1);
-      console.log("random", random);
-      if(random != initialChance) {
-        currentText.push(text);
-        return;
-      }
-      if(!text.match(/[^ ]/)){
-        currentText.push(text);
-      }else{
-        currentText.push(String.fromCharCode(Math.floor(Math.random() * 255)));
-      }
-    });
-    const displayText = currentText.join("");
-    text.innerHTML = displayText;
-    currentText = [];
-    if(Math.floor(Math.random() * 2) == 0){
-      initialChance++;
-    }
-    if(initialChance > 30) {
-      clearInterval(timer);
-      text.innerHTML = initialText;
-    };
-  }, 50);
+function start() {
+    timer = setInterval(() => {
+        initialText.split("").forEach((text) => {
+            const random = Math.floor(Math.random() * initialChance + 1);
+            console.log("random", random);
+            if (random != initialChance) {
+                currentText.push(text);
+                return;
+            }
+            if (!text.match(/[^ ]/)) {
+                currentText.push(text);
+            }
+            else {
+                currentText.push(String.fromCharCode(Math.floor(Math.random() * 255)));
+            }
+        });
+        const displayText = currentText.join("");
+        text.innerHTML = displayText;
+        currentText = [];
+        if (Math.floor(Math.random() * 2) == 0) {
+            initialChance++;
+        }
+        if (initialChance > 30) {
+            clearInterval(timer);
+            text.innerHTML = initialText;
+        }
+    }, 50);
 }
-
 start();
-
